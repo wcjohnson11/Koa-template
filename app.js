37f7bfe3bd2b32039fcfdd5749ad42ad79f72fdc
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-11-30 13:46:17 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var views = require('co-views');
+var koa = require('koa');
+var app = module.exports = koa();
+
+// setup views, appending .ejs
+// when no extname is given to render()
+
+var render = views(__dirname + '/views', { ext: 'ejs' });
+
+// dummy data
+
+var user = {
+  name: {
+    first: 'Will',
+    last: 'Johnson'
+  },
+  species: 'octopus',
+  age: 3
+};
+
+// render
+
+app.use(function *(){
+  this.body = yield render('user', { user: user });
+});
+
+if (!module.parent) app.listen(4000);
